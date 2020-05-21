943e45fe6822ab4a4bd107fc1d96ed0dff38068a
KarenKuan <18055889@myrp.edu.sg>
2020-05-21 23:21:33 +0800

    P05 - Problem Statement

--- app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p05_ndpsongs/MainActivity.java
@@ -2,13 +2,70 @@ package sg.edu.rp.c346.p05_ndpsongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etSingers, etYear;
+    RadioGroup rg;
+    RadioButton rb1, rb2, rb3, rb4, rb5;
+    Button btnInsert, btnShowlist;
+    ArrayList<Song> al;
+    ArrayAdapter<Song> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTitle = findViewById(R.id.etTitle);
+        etSingers = findViewById(R.id.etSingers);
+        etYear = findViewById(R.id.etYear);
+        rg = findViewById(R.id.rgstars);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
+        rb4 = findViewById(R.id.rb4);
+        rb5 = findViewById(R.id.rb5);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnShowlist = findViewById(R.id.btnShowList);
+
+        al = new ArrayList<Song>();
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = etTitle.getText().toString();
+                String singers = etSingers.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertSong(title, singers, year, );
+                dbh.close();
+
+                if (inserted_id != -1){
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnShowlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
